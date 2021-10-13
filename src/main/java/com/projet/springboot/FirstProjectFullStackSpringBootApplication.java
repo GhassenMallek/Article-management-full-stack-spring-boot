f25bf003e17d0a63b3e06a874aa90b87eac2824e
MGhassen30798 <mallekghassen@gmail.com>
2021-10-13 23:42:15 +0100

    Spring boot project with bootstrap -> Crud

---
+++ src/main/java/com/projet/springboot/FirstProjectFullStackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.projet.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectFullStackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectFullStackSpringBootApplication.class, args);
+	}
+
+}
